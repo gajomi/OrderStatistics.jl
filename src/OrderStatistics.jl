a05d8e1ccd9e8970d970ce99f58b49c6f01967e8
Gabriel Joel Mitchell <gajomi271828@gmail.com>
2015-08-15 14:04:01 -0700

    Initial functionality with high level/low level api

--- src/OrderStatistics.jl
+++ src/OrderStatistics.jl
@@ -1,5 +1,21 @@
 module OrderStatistics
 
-# package code goes here
+using Distributions
+using IndependentRandomSequences
+
+import Base:length,rand,mean,max,min,maximum,minimum
+import Distributions:pdf,logpdf,cdf,quantile,insupport
+
+export orderstatistic,orderstatistics,
+        OrderStatistic
+
+include("ScalarOrderStatistic.jl")
+include("generic.jl")
+
+dnames = ["uniform"]
+for dname in dnames
+    include(joinpath("distributions", "$(dname).jl"))
+end
+
 
 end # module
