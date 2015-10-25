a7f90ca5e6899afa4427d7cb59e878f46ba20a92
Gabriel Joel Mitchell <gajomi271828@gmail.com>
2015-10-24 22:30:58 -0700

    continued wranling with dispatch to avoid ambiguity

--- src/ScalarOrderStatistic.jl
+++ src/ScalarOrderStatistic.jl
@@ -11,11 +11,17 @@ typealias IIDScalarOrderStatistic{D<:ValueSupport,T<:UnivariateDistribution} Sca
 typealias IIDContinuousScalarOrderStatistic{T<:ContinuousUnivariateDistribution} IIDScalarOrderStatistic{Continuous,T}
 typealias IIDDiscreteScalarOrderStatistic{T<:DiscreteUnivariateDistribution} IIDScalarOrderStatistic{Discrete,T}
 
-#IID SEQUENCES
+#IID SEQUENCES (having probelsm with Discrete/Continuous DRY due to ambiguity creation)
 minimum{T<:ContinuousUnivariateDistribution}(X::IIDContinuousScalarOrderStatistic{T}) = minimum(X.sequence.d)
+minimum{T<:DiscreteUnivariateDistribution}(X::IIDDiscreteScalarOrderStatistic{T}) = minimum(X.sequence.d)
 maximum{T<:ContinuousUnivariateDistribution}(X::IIDContinuousScalarOrderStatistic{T}) = maximum(X.sequence.d)
+maximum{T<:DiscreteUnivariateDistribution}(X::IIDDiscreteScalarOrderStatistic{T}) = maximum(X.sequence.d)
 
-function cdf{T<:ContinuousUnivariateDistribution}(X::ScalarOrderStatistic{Continuous,IIDRandomSequence{Continuous,T}}, x::Real)
+function cdf{T<:ContinuousUnivariateDistribution}(X::IIDContinuousScalarOrderStatistic{T}, x::Real)
+  P,n,k = cdf(X.sequence.d,x), length(X.sequence), X.order
+  return ccdf(Binomial(n,P),k-1)
+end
+function cdf{T<:DiscreteUnivariateDistribution}(X::IIDDiscreteScalarOrderStatistic{T}, x::Int64)
   P,n,k = cdf(X.sequence.d,x), length(X.sequence), X.order
   return ccdf(Binomial(n,P),k-1)
 end
@@ -25,20 +31,4 @@ function pdf{T<:ContinuousUnivariateDistribution}(X::ScalarOrderStatistic{Contin
   Γ = gamma
   coeff = Γ(n+1)/(Γ(k)*Γ(n-k+1))
   return coeff*P^(k-1)*(1.-P)^(n-k)*p
-end
-
-#INID SEQUENCES
-function cdf(X::ScalarOrderStatistic{Continuous,INIDRandomSequence}, x::Real)
-  #P,n,k = cdf(X.sequence.d,x), length(X.sequence), X.order
-  return 0 #ccdf(Binomial(n,P),k-1)
-end
-
-function pdf(X::ScalarOrderStatistic{Continuous,INIDRandomSequence}, x::Real)
-  #P,p,n,k = cdf(X.sequence.d,x), pdf(X.sequence.d,x),length(X.sequence),X.order
-  #coeff = factorial(n)/(factorial(k-1)*factorial(n-k))
-  return 0 #oeff*P^(k-1)*(1.-P)^(n-k)*p
-end
-
-function max(Xs::INIDRandomSequence)
-  return ScalarOrderStatistic(Xs,length(Xs))
 end
