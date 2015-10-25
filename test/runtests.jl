89ba759ebf711e2fa3951496a2941e234aa9a209
Gabriel Joel Mitchell <gajomi271828@gmail.com>
2015-10-24 22:31:42 -0700

    crude ordering tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,14 @@
+using Distributions
+using IndependentRandomSequences
 using OrderStatistics
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# crude ordering tests
+N = 5
+for X in [Uniform(),Normal()]
+  sequence = IIDRandomSequence(X,N)
+  Ys = orderstatistics(sequence)
+  @test issorted(map(mean,Ys))
+  @test mean(Ys[1]) < mean(X)
+  @test mean(Ys[end]) > mean(X)
+end
