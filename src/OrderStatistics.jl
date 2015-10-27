00036de1828cc53b910086bc0e814694e22cde43
Gabriel Joel Mitchell <gajomi271828@gmail.com>
2015-10-26 21:39:15 -0700

    further refactoring on joint order stats

--- src/OrderStatistics.jl
+++ src/OrderStatistics.jl
@@ -1,7 +1,9 @@
 module OrderStatistics
 
+using Iterators
 using Distributions
 using IndependentRandomSequences
+
 
 import Base:length,rand,mean,max,min,maximum,minimum
 import Distributions:_rand!,pdf,_logpdf,logpdf,cdf,quantile,insupport
@@ -9,7 +11,7 @@ import Distributions:_rand!,pdf,_logpdf,logpdf,cdf,quantile,insupport
 export AbstractOrderStatistic,AbstractScalarOrderStatistic,AbstractJointOrderStatistic,
       ScalarOrderStatistic,JointOrderStatistic,
       sequence,order,orders,
-      orderstatistic,orderstatistics,jointorderstatistic
+      orderstatistic,orderstatistics,jointorderstatistic,jointorderstatistics
 
 include("abstract.jl")
 include("helpers.jl")
--- src/generic.jl
+++ src/generic.jl
@@ -1,7 +1,9 @@
 
-orderstatistic{S<:IIDRandomSequence}(Xs::S,k::Int64) = OrderStatistic(Xs,k)
-orderstatistic{S<:INIDRandomSequence}(Xs::S,k::Int64) = OrderStatistic(Xs,k)
-orderstatistic{T<:ContinuousUnivariateDistribution}(X::T,n::Int64,k::Int64) = orderstatistic(IIDRandomSequence(X,n),k)
+orderstatistic{S<:IIDRandomSequence}(Xs::S,k::Int64) = ScalarOrderStatistic(Xs,k)
+orderstatistic{S<:INIDRandomSequence}(Xs::S,k::Int64) = ScalarOrderStatistic(Xs,k)
+function orderstatistic{T<:ContinuousUnivariateDistribution}(X::T,n::Integer,k::Integer)
+  return orderstatistic(IIDRandomSequence(X,n),k)
+end
 
 orderstatistics{S<:IIDRandomSequence}(Xs::S) = [orderstatistic(Xs,k) for k = 1:length(Xs)]
 orderstatistics{S<:INIDRandomSequence}(Xs::S) = [orderstatistic(Xs,k) for k = 1:length(Xs)]
@@ -13,9 +15,18 @@ min{T<:ContinuousUnivariateDistribution}(X::T,n::Int64) = min(IIDRandomSequence(
 max(Xs::IIDRandomSequence) = orderstatistic(Xs,length(Xs))
 max{T<:ContinuousUnivariateDistribution}(X::T,n::Int64) = max(IIDRandomSequence(X,n))
 
-# jointorderstatistics(X::S,k1::Int64,k2::Int64) #prefer instead
-# jointorderstatistics(X::S,ks::Vector{Int64})
-# jointorderstatistics(X::S)
+jointorderstatistic(X::IIDRandomSequence,orders::Vector{Int64}) = JointOrderStatistic(X,orders)
+jointorderstatistic(X::IIDRandomSequence,k1::Int64,k2::Int64) = jointorderstatistic(X,[k1,k2])
+jointorderstatistic(X::IIDRandomSequence) = jointorderstatistic(X,collect(1:length(X)))
+
+function jointorderstatistics(X::AbstractJointOrderStatistic,n::Integer)
+  return imap(orders->jointorderstatistic(X,orders),combinations(1:length(sequence(X)),n))
+end
+function jointorderstatistics(X::AbstractJointOrderStatistic)
+  N = length(sequence(X))
+  ordercombos = chain([combinations(1:N,k) for k=2:N]...)
+  return imap(orders->jointorderstatistic(X,orders),ordercombos)
+end
 
 # range(Y::JointOrderStatistic)
 # spacings(Y,orders)
