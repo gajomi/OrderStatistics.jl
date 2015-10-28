75f394288beb17288671f02c730a856da06d519c
Gabriel Joel Mitchell <gajomi271828@gmail.com>
2015-10-27 17:44:56 -0700

    less restrictive typing/unimplemented methods for generics

--- src/generic.jl
+++ src/generic.jl
@@ -1,32 +1,60 @@
-
-orderstatistic{S<:IIDRandomSequence}(Xs::S,k::Int64) = ScalarOrderStatistic(Xs,k)
-orderstatistic{S<:INIDRandomSequence}(Xs::S,k::Int64) = ScalarOrderStatistic(Xs,k)
+#
+# univariate orderstatistic methods
+#
+orderstatistic(Xs::MultivariateDistribution,k::Int64) = ScalarOrderStatistic(Xs,k)
 function orderstatistic{T<:ContinuousUnivariateDistribution}(X::T,n::Integer,k::Integer)
   return orderstatistic(IIDRandomSequence(X,n),k)
 end
 
-orderstatistics{S<:IIDRandomSequence}(Xs::S) = [orderstatistic(Xs,k) for k = 1:length(Xs)]
-orderstatistics{S<:INIDRandomSequence}(Xs::S) = [orderstatistic(Xs,k) for k = 1:length(Xs)]
+orderstatistics(Xs::MultivariateDistribution) = [orderstatistic(Xs,k) for k = 1:length(Xs)]
 orderstatistics{T<:ContinuousUnivariateDistribution}(X::T,n::Int64) = orderstatistics(IIDRandomSequence(X,n))
 
-min(Xs::IIDRandomSequence) = orderstatistic(Xs,1)
-min{T<:ContinuousUnivariateDistribution}(X::T,n::Int64) = min(IIDRandomSequence(X,n))
+#
+# joint order statistic methods
+#
+jointorderstatistic(X::MultivariateDistribution,orders::Vector{Int64}) = JointOrderStatistic(X,orders)
+jointorderstatistic(X::MultivariateDistribution) = jointorderstatistic(X,collect(1:length(X)))
 
-max(Xs::IIDRandomSequence) = orderstatistic(Xs,length(Xs))
-max{T<:ContinuousUnivariateDistribution}(X::T,n::Int64) = max(IIDRandomSequence(X,n))
-
-jointorderstatistic(X::IIDRandomSequence,orders::Vector{Int64}) = JointOrderStatistic(X,orders)
-jointorderstatistic(X::IIDRandomSequence,k1::Int64,k2::Int64) = jointorderstatistic(X,[k1,k2])
-jointorderstatistic(X::IIDRandomSequence) = jointorderstatistic(X,collect(1:length(X)))
-
-function jointorderstatistics(X::AbstractJointOrderStatistic,n::Integer)
-  return imap(orders->jointorderstatistic(X,orders),combinations(1:length(sequence(X)),n))
+function jointorderstatistics(X::MultivariateDistribution,n::Integer)
+  return imap(orders->jointorderstatistic(X,orders),combinations(1:length(X),n))
 end
-function jointorderstatistics(X::AbstractJointOrderStatistic)
-  N = length(sequence(X))
+function jointorderstatistics(X::MultivariateDistribution)
+  N = length(X)
   ordercombos = chain([combinations(1:N,k) for k=2:N]...)
   return imap(orders->jointorderstatistic(X,orders),ordercombos)
 end
 
-# range(Y::JointOrderStatistic)
-# spacings(Y,orders)
+#
+# max/min methods
+#
+min(Xs::MultivariateDistribution) = orderstatistic(Xs,1)
+min{T<:ContinuousUnivariateDistribution}(X::T,n::Int64) = min(IIDRandomSequence(X,n))
+
+max(Xs::MultivariateDistribution) = orderstatistic(Xs,length(Xs))
+max{T<:ContinuousUnivariateDistribution}(X::T,n::Int64) = max(IIDRandomSequence(X,n))
+
+#
+# range methods
+#
+function range(Y::MultivariateDistribution)
+  error("No generic implementation available at this time")
+end
+
+#
+# spacing methods
+#
+function spacing(Y::MultivariateDistribution,space::Integer)
+  error("No generic implementation available at this time")
+end
+
+spacings(Y::MultivariateDistribution) = [spacing(Y,space) for space = 1:length(Y)+1]
+
+function jointspacing(Y::MultivariateDistribution,spaces::Vector{Integer})
+  error("No generic implementation available at this time")
+end
+
+function jointspacings(X::MultivariateDistribution)
+  N = length(X)+1
+  spacecombos = chain([combinations(1:N,k) for k=2:N]...)
+  return imap(orders->jointspacing(X,spaces),spacecombos)
+end
